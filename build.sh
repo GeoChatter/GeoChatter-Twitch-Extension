acf18d00eaff6ed5a03f575f5766047d301dc326
rhino <smhmmc98@gmail.com>
2022-08-14 23:38:15 +0300

    Remove unnecessary checks

--- build.sh
+++ build.sh
@@ -7,51 +7,6 @@ then
     exit
 fi
 
-if ! command -v tsc &> /dev/null
-then
-    echo "tsc could not be found. It is required to compile source files."
-    
-    echo ""
-    read -p "Would you like to install 'typescript' now ? (y/n)" tscinstall
-    
-    case $tscinstall in
-	    y|Y)
-            echo ""
-            echo "Installing typescript..."
-            npm install -g typescript
-            ;;
-        *)
-		    echo ""
-		    echo "Please install the typescript manually globally with 'npm install -g typescript'"
-		    echo ""
-		    read -p "Press enter to close the program..."
-		    exit
-		    ;;
-    esac
-fi
-
-if ! command -v node &> /dev/null
-then
-    echo "node could not be found. It is required to build"
-    echo ""
-    read -p "Would you like to install 'node' now ? (y/n)" nodeinstall
-    
-    case $nodeinstall in
-	    y|Y)
-            echo ""
-            echo "Installing node..."
-            npm install -g node
-            ;;
-        *)
-		    echo ""
-		    echo "Please install the typescript manually globally with 'npm install -g node'"
-		    echo ""
-		    read -p "Press enter to close the program..."
-		    exit
-		    ;;
-    esac
-fi
-
 echo ""
 echo "Installing packages..."
 echo ""
